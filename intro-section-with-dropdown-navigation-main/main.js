b8769f3f15e30bd4e07072fa6f1984dc39e0e81c
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2023-08-15 15:15:20 -0400

    added nav toggle

--- intro-section-with-dropdown-navigation-main/main.js
+++ intro-section-with-dropdown-navigation-main/main.js
@@ -2,6 +2,9 @@ const navToggle = document.querySelector('.nav-toggle');
 const navToggleClose = document.querySelector('.nav-toggle-close');
 const nav = document.querySelector('.nav');
 const features = document.querySelector('.nav-link--features');
+const company = document.querySelector('.nav-link--company');
+const featuresList = document.querySelector('.nav-nested--features');
+const companyList = document.querySelector('.nav-nested--company');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('nav--visible');
@@ -9,4 +12,12 @@ navToggle.addEventListener('click', () => {
 
 navToggleClose.addEventListener('click', (e) => {
   nav.classList.toggle('nav--visible');
+});
+
+features.addEventListener('click', (e) => {
+  featuresList.classList.toggle('nav--visible');
+});
+
+company.addEventListener('click', (e) => {
+  companyList.classList.toggle('nav--visible');
 });
