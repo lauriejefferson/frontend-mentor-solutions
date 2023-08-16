09ad30a95c8991adf28b046b3de35393be4a082c
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2023-08-16 10:07:15 -0400

    added styles to hero and navigation

--- intro-section-with-dropdown-navigation-main/main.js
+++ intro-section-with-dropdown-navigation-main/main.js
@@ -5,6 +5,8 @@ const features = document.querySelector('.nav-link--features');
 const company = document.querySelector('.nav-link--company');
 const featuresList = document.querySelector('.nav-nested--features');
 const companyList = document.querySelector('.nav-nested--company');
+const arrowUpOne = document.querySelector('.arrow-up--one');
+const arrowUpTwo = document.querySelector('.arrow-up--two');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('nav--visible');
@@ -16,8 +18,10 @@ navToggleClose.addEventListener('click', (e) => {
 
 features.addEventListener('click', (e) => {
   featuresList.classList.toggle('nav--visible');
+  arrowUpOne.classList.toggle('rotate');
 });
 
 company.addEventListener('click', (e) => {
   companyList.classList.toggle('nav--visible');
+  arrowUpTwo.classList.toggle('rotate');
 });
