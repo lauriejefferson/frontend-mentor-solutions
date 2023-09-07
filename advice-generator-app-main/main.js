0283958fb548f16951489ce68f9bc9f989a9fc4c
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2023-09-07 15:35:00 -0400

    added btn event listener

--- advice-generator-app-main/main.js
+++ advice-generator-app-main/main.js
@@ -1,17 +1,15 @@
 const url = 'https://api.adviceslip.com/advice';
 const adviceNum = document.querySelector('.advice__number');
-const adviceText = document.querySelector('advice__text');
+const adviceText = document.querySelector('.advice__text');
 const btn = document.querySelector('button');
-
-let advice = '';
 
 async function getAdvice() {
   const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  let data = await response.json();
+  adviceNum.textContent = `Advice #${data.slip.id}`;
+  adviceText.textContent = `${data.slip.advice}`;
 }
 
-advice = await getAdvice();
-console.log(advice);
-
-adviceNum.textContent = `Advice #${advice.slip.id}`;
+btn.addEventListener('click', async () => {
+  await getAdvice();
+});
