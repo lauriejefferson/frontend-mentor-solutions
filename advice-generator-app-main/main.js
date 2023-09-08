83f25e5d7e3036967796488ff601b161970a05ba
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2023-09-08 18:16:46 -0400

    added default string to advice text

--- advice-generator-app-main/main.js
+++ advice-generator-app-main/main.js
@@ -3,13 +3,19 @@ const adviceNum = document.querySelector('.advice__number');
 const adviceText = document.querySelector('.advice__text');
 const btn = document.querySelector('button');
 
+adviceNum.innerHTML = 'Advice #117';
+adviceText.innerHTML =
+  "It is easy to sit up and take notice, what's difficult is getting up and taking action.";
+
+let result = '';
 async function getAdvice() {
   const response = await fetch(url);
-  let data = await response.json();
-  adviceNum.textContent = `Advice #${data.slip.id}`;
-  adviceText.textContent = `${data.slip.advice}`;
+  const data = await response.json();
+  return data;
 }
 
 btn.addEventListener('click', async () => {
-  await getAdvice();
+  result = await getAdvice();
+  adviceNum.innerHTML = `Advice #${result.slip.id}`;
+  adviceText.innerHTML = `${result.slip.advice}`;
 });
