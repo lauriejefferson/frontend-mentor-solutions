8edbffbed87339d06dfb6cfd428086ea0e2a666b
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2023-11-22 13:08:43 -0500

    added vue component

--- fylo-dark-theme-landing-page-master/my-component.js
+++ fylo-dark-theme-landing-page-master/my-component.js
@@ -1,8 +1,13 @@
 import { ref } from 'vue';
 export default {
-  setup() {
-    const count = ref(0);
-    return { count };
-  },
-  template: `<div>count is {{ count }}</div>`,
+  setup() {},
+  template: `<div class="sign-up-form">
+    <h1 class="sign-up-form__title">Get early access today</h1>
+    <p class="sign-up-form__text"> It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+    </p>
+    <input type="text" placeholder="email@example.com"/>
+    <button class="sign-up-form__submit">Get Started For Free </button>
+  </div>`,
 };
