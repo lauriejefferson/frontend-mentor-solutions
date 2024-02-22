14f446c39960e85bd2a2d941e5629fb466091c6f
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-02-22 13:00:01 -0500

    fixes styling on sunnyside-agency-landing-page

--- sunnyside-agency-landing-page-main/main.js
+++ sunnyside-agency-landing-page-main/main.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav');
+const heroTitle = document.querySelector('.hero-title');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('nav--visible');
+  heroTitle.classList.toggle('hero-title--hidden');
 });
