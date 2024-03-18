8c50d29a126f2b262ff3a717c7e9c67b0344426a
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-03-18 11:30:10 -0400

    fixes styling on flylo landing page

--- fylo-dark-theme-landing-page-master/main.js
+++ fylo-dark-theme-landing-page-master/main.js
@@ -1,10 +1,12 @@
+const form = document.querySelector('form');
 const input = document.querySelector('input');
-const btn = document.querySelector('.early-access__submit');
 const errorMessage = document.querySelector('.error-message');
 
-btn.addEventListener('click', (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   if (input.value && input.validity.patternMismatch === true) {
     errorMessage.style.visibility = 'visible';
+  } else {
+    errorMessage.style.visibility = 'hidden';
   }
 });
